90963d8847b2993e4270a57532e6410e578d5ff1
Ronja <git@ronja.space>
2020-12-25 09:12:13 +0100

    Last Day

---
+++ Day25/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Day25
+{
+    static class Program
+    {
+        static void Main() {
+            var lines = File.ReadAllLines("input.txt");
+            var doorPub = int.Parse(lines[0]);
+            var cardPub = int.Parse(lines[1]);
+            var doorLoop = FindLoopSize(7, doorPub);//optional
+            var cardLoop = FindLoopSize(7, cardPub);
+            var encryptCard = Transform(doorPub, cardLoop);
+            var encryptDoor = Transform(cardPub, doorLoop); //optional
+            Console.WriteLine($"The door loops {doorLoop} times, the card loops {cardLoop} times and the encryption key" +
+                              $"is {encryptCard}");
+        }
+
+        static long Transform(int subject, int loop) {
+            long key = 1;
+            for (int i = 0; i < loop; i++) {
+                key *= subject;
+                key %= 20201227;
+            }
+            return key;
+        }
+
+        static int FindLoopSize(int subject, int key) {
+            int loopSize = 0;
+            while (key != 1) {
+                loopSize++;
+                //undo modulo until stuff works
+                while (key % subject != 0)
+                    key += 20201227;
+                key /= subject;
+            }
+            return loopSize;
+        }
+    }
+}
